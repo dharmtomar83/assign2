b33c1347e61410a07e5af464f587b7f187ee4dba
dharm <dharm@LAPTOP-OROP1HV7>
2023-08-28 14:11:39 +0530

    Add project files.

---
+++ assign2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class assign2
+{
+   
+    public string AccountNumber { get; set; }
+    public string AccountHolderName { get; set; }
+    public double Balance { get; private set; }
+
+    
+    public assign2(string accountNumber, string accountHolderName, double initialBalance)
+    {
+        AccountNumber = accountNumber;
+        AccountHolderName = accountHolderName;
+        Balance = initialBalance;
+    }
+
+    
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited ${amount}. New balance: ${Balance}");
+        }
+        else
+        {
+            Console.WriteLine("Deposit amount must be greater than 0.");
+        }
+    }
+
+    
+    public void Withdraw(double amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrawn ${amount}. New balance: ${Balance}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid withdrawal amount or insufficient balance.");
+        }
+    }
+
+    
+    public void DisplayAccountDetails()
+    {
+        Console.WriteLine($"Account Number: {AccountNumber}");
+        Console.WriteLine($"Account Holder: {AccountHolderName}");
+        Console.WriteLine($"Balance: ${Balance}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        assign2 account = new assign2("123456789", "John Doe", 1000.0);
+
+        
+        account.DisplayAccountDetails();
+
+        
+        account.Deposit(500.0);
+        account.Withdraw(200.0);
+
+        
+        account.DisplayAccountDetails();
+    }
+}
